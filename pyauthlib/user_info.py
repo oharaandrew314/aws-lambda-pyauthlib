ff8ea6c8d0434120372cbf72287589acd99844ee
Andrew O'Hara <andrew@andrewohara.io>
2017-11-04 17:40:17 -0400

    fix python 2 compatibility

--- pyauthlib/user_info.py
+++ pyauthlib/user_info.py
@@ -2,6 +2,8 @@
 
 import json
 
+from six import string_types
+
 
 class UserInfo(object):
     '''User Info'''
@@ -9,13 +11,13 @@ class UserInfo(object):
     @classmethod
     def from_json(cls, json_data):
         '''Parse User-Info from json'''
-        if isinstance(json_data, str):
+        if isinstance(json_data, string_types):
             json_data = json.loads(json_data)
 
         return cls(json_data['user_id'], json_data['authorities'])
 
     def __init__(self, user_id, authorities):
-        if isinstance(authorities, str):
+        if isinstance(authorities, string_types):
             authorities = authorities.split(',')
         elif not isinstance(authorities, (list, tuple)):
             raise ValueError('Authorites must be comma-seperated, or a list')
