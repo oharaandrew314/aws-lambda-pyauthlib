d4c8900a41a49b34fc9100991e3c1b8dc56085fd
Andrew O'Hara <andrew@andrewohara.io>
2017-11-11 16:10:38 -0500

    Add jwt example

---
+++ examples/test_jwt.py
@@ -0,0 +1,122 @@
+'''Authorizer Example with JWT Token'''
+
+import jwt
+from jwt.exceptions import DecodeError
+
+from pyauthlib import UserInfo, AuthPolicy, HttpMethod, parse_event
+
+JWT_SECRET = 'secret'
+
+USER_JWT_TOKEN = jwt.encode(
+    dict(user_id='user', authorities='ROLE_USER,ROLE_RANDOM_AUTHORITY'),
+    JWT_SECRET
+).decode('utf-8')
+
+ADMIN_JWT_TOKEN = jwt.encode(
+    dict(user_id='admin', authorities='ROLE_ADMIN'),
+    JWT_SECRET
+).decode('utf-8')
+
+
+def lambda_handler(event, _context):
+    '''Decodes JWT token into UserInfo and returns the policy.
+
+        Unauthorized users are denied all access.
+        Users are allowed read access to all resources.
+        Admins are allowed full access to all resources.
+    '''
+    event = parse_event(event)
+
+    try:
+        identity = jwt.decode(event.access_token, JWT_SECRET)
+        user_info = UserInfo(identity['user_id'], identity['authorities'])
+    except DecodeError:
+        user_info = None
+
+    policy = AuthPolicy(user_info)
+    if not user_info:
+        policy.deny(event.arn(method=HttpMethod.ALL, resource='*'))
+    elif 'ROLE_ADMIN' in user_info.authorities:
+        policy.allow(event.arn(method=HttpMethod.ALL, resource='*'))
+    else:
+        policy.allow(event.arn(method=HttpMethod.GET, resource='*'))
+
+    return policy.build()
+
+
+def test_unauthenticated():
+    '''Test the policy of an unauthenticated user'''
+    event = dict(
+        authorizationToken='Bearer toll_troll',
+        methodArn='arn:aws:execute-api:us-east-1:account_id:rest_api_id/prod/GET/'
+    )
+
+    assert lambda_handler(event, None) == dict(
+        principalId='anonymous',
+        context=dict(
+            user_id='anonymous',
+            authorities='ROLE_ANONYMOUS'
+        ),
+        policyDocument=dict(
+            Version='2012-10-17',
+            Statement=[
+                dict(
+                    Action='execute-api:Invoke',
+                    Effect='Deny',
+                    Resource=['arn:aws:execute-api:us-east-1:account_id:rest_api_id/prod/*/*']
+                )
+            ]
+        )
+    )
+
+
+def test_user():
+    '''Test the policy of a read-only user'''
+    event = dict(
+        authorizationToken='Bearer ' + USER_JWT_TOKEN,
+        methodArn='arn:aws:execute-api:us-east-1:account_id:rest_api_id/prod/GET/'
+    )
+
+    assert lambda_handler(event, None) == dict(
+        principalId='user',
+        context=dict(
+            user_id='user',
+            authorities='ROLE_USER,ROLE_RANDOM_AUTHORITY'
+        ),
+        policyDocument=dict(
+            Version='2012-10-17',
+            Statement=[
+                dict(
+                    Action='execute-api:Invoke',
+                    Effect='Allow',
+                    Resource=['arn:aws:execute-api:us-east-1:account_id:rest_api_id/prod/GET/*']
+                )
+            ]
+        )
+    )
+
+
+def test_admin():
+    '''Test the policy of an admin user'''
+    event = dict(
+        authorizationToken='Bearer ' + ADMIN_JWT_TOKEN,
+        methodArn='arn:aws:execute-api:us-east-1:account_id:rest_api_id/prod/GET/'
+    )
+
+    assert lambda_handler(event, None) == dict(
+        principalId='admin',
+        context=dict(
+            user_id='admin',
+            authorities='ROLE_ADMIN'
+        ),
+        policyDocument=dict(
+            Version='2012-10-17',
+            Statement=[
+                dict(
+                    Action='execute-api:Invoke',
+                    Effect='Allow',
+                    Resource=['arn:aws:execute-api:us-east-1:account_id:rest_api_id/prod/*/*']
+                )
+            ]
+        )
+    )
